c94d9725eb63371559ac2dab6241f9fd6f2da5a7
Josh King <jking@chambana.net>
2019-09-12 08:38:28 -0400

    Initial commit.

---
+++ murakami/__main__.py
@@ -0,0 +1,80 @@
+import configargparse
+import logging
+from .server import MurakamiServer
+
+
+def main():
+    parser = configargparse.ArgParser(
+        description="The Murakami network test runner.")
+    parser.add(
+        "-p",
+        "--port",
+        type=int,
+        default=80,
+        env_var="MURAKAMI_PORT",
+        help="The port to listen on for incoming connections (default: 80).",
+    )
+    parser.add(
+        "-n",
+        "--hostname",
+        nargs="?",
+        env_var="MURAKAMI_HOSTNAME",
+        help="The mDNS hostname for WebThings (default: automatic).",
+    )
+    parser.add(
+        "-s",
+        "--ssl-options",
+        nargs="?",
+        dest="ssl_options",
+        env_var="MURAKAMI_SSL_OPTIONS",
+        help="SSL options for the WebThings server (default: none).",
+    )
+    parser.add(
+        "-r",
+        "--additional-routes",
+        nargs="?",
+        dest="additional_routes",
+        env_var="MURAKAMI_ADDITIONAL_ROUTES",
+        help=
+        "List of additional routes for the WebThings server (default: none).",
+    )
+    parser.add(
+        "-b",
+        "--base-path",
+        default="",
+        dest="base_path",
+        env_var="MURAKAMI_BASE_PATH",
+        help="Base URL path to use, rather than '/' (default: '').",
+    )
+    parser.add(
+        "-l",
+        "--log",
+        dest="loglevel",
+        default="DEBUG",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+        env_var="MURAKAMI_LOGLEVEL",
+        help="Set the logging level",
+    )
+    settings = parser.parse_args()
+
+    logging.basicConfig(
+        level=settings.loglevel,
+        format="%(asctime)s %(filename)s:%(lineno)s %(levelname)s %(message)s",
+    )
+
+    server = MurakamiServer(
+        port=settings.port,
+        hostname=settings.hostname,
+        ssl_options=settings.ssl_options,
+        additional_routes=settings.additional_routes,
+        base_path=settings.base_path,
+    )
+
+    try:
+        server.start()
+    except KeyboardInterrupt:
+        server.stop()
+
+
+if __name__ == "__main__":
+    main()
---
+++ murakami/server.py
@@ -0,0 +1,35 @@
+import logging
+import pkg_resources
+from webthing import WebThingServer, MultipleThings
+
+
+class MurakamiServer:
+    def __init__(
+            self,
+            port=80,
+            hostname=None,
+            ssl_options=None,
+            additional_routes=None,
+            base_path="",
+    ):
+        self.runners = {
+            entry_point.name: entry_point.load()()
+            for entry_point in pkg_resources.iter_entry_points(
+                "murakami.runner")
+        }
+        self.server = WebThingServer(
+            MultipleThings(self.runners.values(), "Murakami"),
+            port=port,
+            hostname=hostname,
+            ssl_options=ssl_options,
+            additional_routes=additional_routes,
+            base_path=base_path,
+        )
+
+    def start(self):
+        logging.info("Starting the WebThing server.")
+        self.server.start()
+
+    def stop(self):
+        logging.info("Stopping the WebThing server.")
+        self.server.stop()
