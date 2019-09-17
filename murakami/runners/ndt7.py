48c4472341967882ff1aba6ce0db2293fc36c205
Rae <amanda.gaines@gmail.com>
2019-09-17 16:07:49 -0400

    fix syntax and add info

--- murakami/runners/ndt7.py
+++ murakami/runners/ndt7.py
@@ -1,1 +1,38 @@
-# This is just a stub
+from __future__ import division, print_function
+from webthing import (Action, Event, MultipleThings, Property, Thing, Value,
+                      WebThingServer)
+import logging
+import random
+import time
+import tornado.ioloop
+import uuid
+
+def run_server():
+    # Create a thing that represents a dimmable light
+    light = ExampleDimmableLight()
+
+    # Create a thing that represents a humidity sensor
+    sensor = FakeGpioHumiditySensor()
+
+    # If adding more than one thing, use MultipleThings() with a name.
+    # In the single thing case, the thing's name will be broadcast.
+    server = WebThingServer(MultipleThings([light, sensor],
+                                           'LightAndTempDevice'),
+                            port=8888)
+    try:
+        logging.info('starting the server')
+        server.start()
+    except KeyboardInterrupt:
+        logging.debug('canceling the sensor update looping task')
+        sensor.cancel_update_level_task()
+        logging.info('stopping the server')
+        server.stop()
+        logging.info('done')
+
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        level=10,
+        format="%(asctime)s %(filename)s:%(lineno)s %(levelname)s %(message)s"
+    )
+    run_server()
