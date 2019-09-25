d1d199b03d3345c11d4281fe7af7bc18d6594169
Rae <amanda.gaines@gmail.com>
2019-09-25 16:25:12 -0400

    begin to build dash runner and lint python

--- murakami/runners/speedtest.py
+++ murakami/runners/speedtest.py
@@ -8,74 +8,74 @@ import uuid
 
 class RunSpeedtest(Action):
 
-  def __init__(self, thing, input_):
-    Action.__init__(self, uuid.uuid4().hex, thing, 'run', input_=input_)
-    print(('input: '),input_)
+    def __init__(self, thing, input_):
+        Action.__init__(self, uuid.uuid4().hex, thing, 'run', input_=input_)
+        print(('input: '),input_)
 
-  def perform_action(self):
-    print('perform speedtest action')
+    def perform_action(self):
+          print('perform speedtest action')
 
 class SpeedtestClient(Thing):
-  """Run Speedtest.net tests."""
+    """Run Speedtest.net tests."""
 
-  def __init__(self):
+    def __init__(self):
 
-    Thing.__init__(
-      self,
-      'urn:dev:ops:ndt7-client',
-      'Speedtest Client',
-      ['OnOffSwitch', 'Client'],
-      'A client running Speedtest tests'
-    )
+        Thing.__init__(
+            self,
+            'urn:dev:ops:ndt7-client',
+            'Speedtest Client',
+            ['OnOffSwitch', 'Client'],
+            'A client running Speedtest tests'
+        )
 
-    self.run_test()
+        self.run_test()
 
-    self.add_property(
-      Property(self,
-        'on',
-        Value(True, lambda v: print('On-State is now', v)),
-        metadata={
-          '@type': 'OnOffProperty',
-          'title': 'On/Off',
-          'type': 'boolean',
-          'description': 'Whether the client is running',
-        }))
+        self.add_property(
+            Property(self,
+                'on',
+                Value(True, lambda v: print('On-State is now', v)),
+                metadata={
+                  '@type': 'OnOffProperty',
+                  'title': 'On/Off',
+                  'type': 'boolean',
+                  'description': 'Whether the client is running',
+                }))
 
-    self.add_available_action(
-      'run',
-      {
-        'title': 'Run',
-        'description': 'Run tests',
-        'input': {
-          'type': 'object',
-          'required': [
-            'download',
-            'upload'
-          ],
-          'properties': {
-            'download': {
-              'type': 'integer',
-              'minimum': 0,
-              'unit': 'Mbit/s',
+        self.add_available_action(
+            'run',
+            {
+                'title': 'Run',
+                'description': 'Run tests',
+                'input': {
+                    'type': 'object',
+                    'required': [
+                        'download',
+                        'upload'
+                    ],
+                    'properties': {
+                        'download': {
+                            'type': 'integer',
+                            'minimum': 0,
+                            'unit': 'Mbit/s',
+                        },
+                        'upload': {
+                            'type': 'integer',
+                            'minimum': 0,
+                            'unit': 'Mbit/s',
+                        },
+                    },
+                },
             },
-            'upload': {
-              'type': 'integer',
-              'minimum': 0,
-              'unit': 'Mbit/s',
-            },
-          },
-        },
-      },
-      RunSpeedtest)
+            RunSpeedtest)
 
-    self.add_available_event(
-      'error',
-      {
-        'description':
-        'There was an error running the tests',
-        'type': 'string',
-        'unit': 'error',
-      })
+        self.add_available_event(
+            'error',
+            {
+                'description':
+                'There was an error running the tests',
+                'type': 'string',
+                'unit': 'error',
+            })
 
-  def run_test(self):
-    os.system('speedtest-cli')
+    def run_test(self):
+        os.system('speedtest-cli')
