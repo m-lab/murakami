dad1a00b124d469f827f2c513db78be67f8d0768
Rae <rae@Raes-MacBook-Pro.local>
2019-10-01 15:28:49 -0400

    update runners to use MurakamiRunner superclass

--- murakami/runners/dash.py
+++ murakami/runners/dash.py
@@ -1,72 +1,89 @@
-from __future__ import division, print_function
-from webthing import (Action, Event, Property, Thing, Value)
-
 import logging
 import os
-import time
+import shutil
 import uuid
+from webthing import Action, Event, Property, Thing, Value
 
-class DashClient(Thing):
-  """Run Dash.net tests."""
+from murakami.errors import RunnerError
+from murakami.runner import MurakamiRunner
 
-  def __init__(self):
+logger = logging.getLogger(__name__)
 
-    Thing.__init__(
-      self,
-      'urn:dev:ops:ndt7-client',
-      'Dash Client',
-      ['OnOffSwitch', 'Client'],
-      'A client running Dash tests'
-    )
 
-    self.run_test()
+class RunDash(Action):
 
-    self.add_property(
-      Property(self,
-        'on',
-        Value(True, lambda v: print('On-State is now', v)),
-        metadata={
-          '@type': 'OnOffProperty',
-          'title': 'On/Off',
-          'type': 'boolean',
-          'description': 'Whether the client is running',
-        }))
+  def __init__(self, thing, input_):
+    Action.__init__(self, uuid.uuid4().hex, thing, "run", input_=input_)
+    print(("input: "),input_)
 
-    self.add_available_action(
-      'run',
-      {
-        'title': 'Run',
-        'description': 'Run tests',
-        'input': {
-          'type': 'object',
-          'required': [
-            'download',
-            'upload'
-          ],
-          'properties': {
-            'download': {
-              'type': 'integer',
-              'minimum': 0,
-              'unit': 'Mbit/s',
+  def perform_action(self):
+    print("perform dash action")
+
+class DashClient(MurakamiRunner):
+    """Run Dash tests."""
+    def __init__(self, config=None, data_cb=None):
+        super().__init__(name="dash", config=config, data_cb=data_cb)
+
+        self._thing = Thing(
+            "urn:dev:ops:dash-client",
+            "Dash Client",
+            ["OnOffSwitch", "Client"],
+            "A client running Dash tests"
+        )
+
+
+        self._thing.add_property(
+            Property(
+                self,
+                "on",
+                Value(True, lambda v: print("On-State is now", v)),
+                metadata={
+                    "@type": "OnOffProperty",
+                    "title": "On/Off",
+                    "type": "boolean",
+                    "description": "Whether the client is running",
+                }
+            )
+        )
+
+        self._thing.add_available_action(
+            "run",
+            {
+                "title": "Run",
+                "description": "Run tests",
+                "input": {
+                    "type": "object",
+                    "required": ["download", "upload"],
+                    "properties": {
+                        "download": {
+                            "type": "integer",
+                            "minimum": 0,
+                            "unit": "Mbit/s",
+                        },
+                        "upload": {
+                            "type": "integer",
+                            "minimum": 0,
+                            "unit": "Mbit/s",
+                        },
+                    },
+                },
             },
-            'upload': {
-              'type': 'integer',
-              'minimum': 0,
-              'unit': 'Mbit/s',
-            },
-          },
-        },
-      },
-      RunDash)
+            RunDash
+        )
 
-    self.add_available_event(
-      'error',
+    self._thing.add_available_event(
+      "error",
       {
-        'description':
-        'There was an error running the tests',
-        'type': 'string',
-        'unit': 'error',
+        "description":
+        "There was an error running the tests",
+        "type": "string",
+        "unit": "error",
       })
 
   def run_test(self):
-    os.system('dash')
+    if shutil.which("dash") is not None:
+        os.system("dash")
+    else:
+        raise RunnerError(
+            "dash",
+            "Executable does not exist, please install dash.")
--- murakami/runners/speedtest.py
+++ murakami/runners/speedtest.py
@@ -1,73 +1,87 @@
-from __future__ import division, print_function
-from webthing import (Action, Event, Property, Thing, Value)
-
 import logging
 import os
-import time
+import shutil
 import uuid
+from webthing import Action, Event, Property, Thing, Value
+
+from murakami.errors import RunnerError
+from murakami.runner import MurakamiRunner
+
+logger = logging.getLogger(__name__)
 
 
-class SpeedtestClient(Thing):
+class RunSpeedtest(Action):
+
+    def __init__(self, thing, input_):
+        Action.__init__(self, uuid.uuid4().hex, thing, "run", input_=input_)
+        print(("input: "),input_)
+
+    def perform_action(self):
+        logger.info("Performing Speeedtest.net test")
+        self.thing.start_test()
+
+class SpeedtestClient(MurakamiRunner):
     """Run Speedtest.net tests."""
 
-    def __init__(self):
+    def __init__(self, config=None, data_cb=None):
+        super().__init__(name="speedtest", config=config, data_cb=data_cb)
 
-        Thing.__init__(
-            self,
-            'urn:dev:ops:ndt7-client',
-            'Speedtest Client',
-            ['OnOffSwitch', 'Client'],
-            'A client running Speedtest tests'
+        self._thing = Thing(
+            "urn:dev:ops:speedtest-client",
+            "Speedtest Client",
+            ["OnOffSwitch", "Client"],
+            "A client running Speedtest.net tests"
         )
 
-        self.run_test()
-
-        self.add_property(
-            Property(self,
-                'on',
-                Value(True, lambda v: print('On-State is now', v)),
+        self._thing.add_property(
+            Property(
+                self,
+                "on",
+                Value(True, lambda v: print("On-State is now", v)),
                 metadata={
-                  '@type': 'OnOffProperty',
-                  'title': 'On/Off',
-                  'type': 'boolean',
-                  'description': 'Whether the client is running',
+                  "@type": "OnOffProperty",
+                  "title": "On/Off",
+                  "type": "boolean",
+                  "description": "Whether the client is running",
                 }))
 
-        self.add_available_action(
-            'run',
+        self._thing.add_available_action(
+            "run",
             {
-                'title': 'Run',
-                'description': 'Run tests',
-                'input': {
-                    'type': 'object',
-                    'required': [
-                        'download',
-                        'upload'
-                    ],
-                    'properties': {
-                        'download': {
-                            'type': 'integer',
-                            'minimum': 0,
-                            'unit': 'Mbit/s',
+                "title": "Run",
+                "description": "Run tests",
+                "input": {
+                    "type": "object",
+                    "required": ["download", "upload"],
+                    "properties": {
+                        "download": {
+                            "type": "integer",
+                            "minimum": 0,
+                            "unit": "Mbit/s",
                         },
-                        'upload': {
-                            'type': 'integer',
-                            'minimum': 0,
-                            'unit': 'Mbit/s',
+                        "upload": {
+                            "type": "integer",
+                            "minimum": 0,
+                            "unit": "Mbit/s",
                         },
                     },
                 },
             },
             RunSpeedtest)
 
-        self.add_available_event(
-            'error',
+        self._thing.add_available_event(
+            "error",
             {
-                'description':
-                'There was an error running the tests',
-                'type': 'string',
-                'unit': 'error',
+                "description":
+                "There was an error running the tests",
+                "type": "string",
+                "unit": "error",
             })
 
-    def run_test(self):
-        os.system('speedtest-cli')
+    def start_test(self):
+        if shutil.which("speedtest-cli") is not None:
+            os.system("speedtest-cli")
+        else:
+            raise RunnerError(
+                "speedtest",
+                "Executable does not exist, please install speedtest-cli.")
