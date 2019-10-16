3acc1a81d9b2cce207bc6dd987b742ac595656be
Rae <rae.rudie@gmail.com>
2019-10-16 14:55:14 -0400

    add flag skip invalid json output from ndt5 runner

--- murakami/runners/libndt5.py
+++ murakami/runners/libndt5.py
@@ -85,4 +85,4 @@ class LibndtClient(MurakamiRunner):
                 "libndt",
                 "Executable libndt-client does not exist, please install libndt.",
             )
-        return [*reader.iter(allow_none=True, skip_empty=True)]
+        return [*reader.iter(allow_none=True, skip_empty=True, skip_invalid=True)]
