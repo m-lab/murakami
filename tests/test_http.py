5f3fa0e84e96f3df6c0fd32b6b122ffdb363f48a
Roberto D'Auria <roberto@measurementlab.net>
2020-05-29 11:32:02 +0200

    Fix tests.

--- tests/test_http.py
+++ tests/test_http.py
@@ -18,7 +18,7 @@ def test_push_response_ok(mock_post):
     mock_post.return_value.json.return_value = RESPONSE_SUCCESS_JSON
 
     exporter = HTTPExporter("test", config={'url': 'http://testurl'})
-    assert exporter.push("ndt5", {'TestName': 'ndt5'}, time.time()) is True
+    assert exporter.push("ndt5", '{"TestName": "ndt5"}', time.time()) is True
 
 @patch('requests.post')
 def test_push_response_error(mock_post):
@@ -26,4 +26,4 @@ def test_push_response_error(mock_post):
     mock_post.return_value.json.return_value = RESPONSE_FAILURE_JSON
 
     exporter = HTTPExporter("test", config={'url': 'http://testurl'})
-    assert exporter.push("ndt5", {'TestName': 'ndt5'}, time.time()) is False
+    assert exporter.push("ndt5", '{"TestName": "ndt5"}', time.time()) is False
