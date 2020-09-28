3ac10f7fde2216fd2d3546eca65216803fbc9739
Josh King <jking@chambana.net>
2020-09-28 10:16:28 -0400

    Fix content-type handling for JSON.

--- murakami/exporters/http.py
+++ murakami/exporters/http.py
@@ -11,13 +11,14 @@ logger = logging.getLogger(__name__)
 
 class HTTPExporter(MurakamiExporter):
     """This exporter sends data to an HTTP endpoint."""
+
     def __init__(
-            self,
-            name="",
-            location=None,
-            network_type=None,
-            connection_type=None,
-            config=None,
+        self,
+        name="",
+        location=None,
+        network_type=None,
+        connection_type=None,
+        config=None,
     ):
         super().__init__(
             name=name,
@@ -32,21 +33,18 @@ class HTTPExporter(MurakamiExporter):
     def push(self, test_name="", data=None, timestamp=None):
         # Make a JSON payload with the expected format
         data = json.loads(data)
-        json_data = {
-            'apiVersion': 1,
-            'data': data
-        }
+        data = {k: v for k, v in data.items() if v is not None}
+        json_data = {"apiVersion": 1, "data": data}
 
         # POST the payload.
-        resp = requests.post(self._url, data=json.dumps(json_data))
+        resp = requests.post(self._url, json=json_data)
+        logging.debug("Exporting data: {}".format(json.dumps(json_data)))
         if not resp.ok:
-            logger.error("Exporting to HTTP endpoint failed: {}".format(
-                resp.json()
-            ))
+            logger.error("Exporting to HTTP endpoint failed: {}".format(resp.json()))
             return False
-        logger.info("Test data successfully sent to {}. Response: {}"
-            .format(
-                self._url,
-                resp.json()
-            ))
+        logger.info(
+            "Test data successfully sent to {}. Response: {}".format(
+                self._url, resp.json()
+            )
+        )
         return True
